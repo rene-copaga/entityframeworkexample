319ae3bdbd310cce31c0773306b4a6e073147ba3
Rene Copaga <rene.copaga@gmail.com>
2021-04-25 19:21:10 -0400

    More Entity Schema Attributes

--- EFCore5WebApp.Core/Entities/Person.cs
+++ EFCore5WebApp.Core/Entities/Person.cs
@@ -7,13 +7,19 @@ namespace EFCore5WebApp.Core.Entities
     [Table("Persons", Schema = "dbo")]
     public class Person
     {
+        [Column("Person_Id")]
         public int Id { get; set; }
         [Required]
         [MaxLength(255)]
+        [Column(TypeName = "varchar(255)")]
         public string FirstName { get; set; }
         [Required]
         [MaxLength(255)]
         public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName => $"{FirstName} {LastName}";
+
         [Required]
         public string EmailAddress { get; set; }
         public List<Address> Addresses { get; set; } = new List<Address>();
