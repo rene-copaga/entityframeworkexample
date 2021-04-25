c873b1f931d6fb5ba92c28cb6c45deb5adb127f1
Rene Copaga <rene.copaga@gmail.com>
2021-04-25 18:36:35 -0400

    Entity Property Constraints

--- EFCore5WebApp.Core/Entities/Person.cs
+++ EFCore5WebApp.Core/Entities/Person.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EFCore5WebApp.Core.Entities
 {
     public class Person
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string FirstName { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string LastName { get; set; }
+        [Required]
         public string EmailAddress { get; set; }
         public List<Address> Addresses { get; set; } = new List<Address>();
     }
