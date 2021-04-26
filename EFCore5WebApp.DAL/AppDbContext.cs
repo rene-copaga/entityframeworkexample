da69c01de21b5bd669a88316d91a4bc1d8aa6ece
Rene Copaga <rene.copaga@gmail.com>
2021-04-25 22:20:06 -0400

    Connecting to Our Database

--- EFCore5WebApp.DAL/AppDbContext.cs
+++ EFCore5WebApp.DAL/AppDbContext.cs
@@ -4,5 +4,12 @@ namespace EFCore5WebApp.DAL
 {
     public class AppDbContext: DbContext
     {
+        public AppDbContext() : base()
+        {
+        }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
     }
 }
