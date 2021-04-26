0958a0f1b4442c7e7c1063e75d87f4af41a89761
Rene Copaga <rene.copaga@gmail.com>
2021-04-25 22:35:25 -0400

    Accessing Entities in a Database Context

--- EFCore5WebApp.DAL/AppDbContext.cs
+++ EFCore5WebApp.DAL/AppDbContext.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EFCore5WebApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCore5WebApp.DAL
 {
     public class AppDbContext: DbContext
     {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<LookUp> LookUps { get; set; }
+
         public AppDbContext() : base()
         {
         }
