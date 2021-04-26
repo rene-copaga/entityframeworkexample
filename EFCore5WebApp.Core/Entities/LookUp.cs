c38dbeddcdd9eff0df09dffd6c407c4b48e4aad8
Rene Copaga <rene.copaga@gmail.com>
2021-04-25 20:59:41 -0400

    Enum Mapping

--- EFCore5WebApp.Core/Entities/LookUp.cs
+++ EFCore5WebApp.Core/Entities/LookUp.cs
@@ -2,11 +2,17 @@
 
 namespace EFCore5WebApp.Core.Entities
 {
+    public enum LookUpType
+    {
+        State = 0,
+        Country = 1
+    }
+
     public class LookUp
     {
         [Key]
         public string Code { get; set; }
         public string Description { get; set; }
-        public int LookUpType { get; set; }
+        public LookUpType LookUpType { get; set; }
     }
 }
