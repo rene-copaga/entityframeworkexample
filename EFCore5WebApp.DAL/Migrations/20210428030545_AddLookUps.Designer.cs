b6c8ee67147c95108b49f659653f54658e1a0fa1
Rene Copaga <rene.copaga@gmail.com>
2021-04-27 23:06:18 -0400

    Populating Lookup Data 2

--- EFCore5WebApp.DAL/Migrations/20210428030545_AddLookUps.Designer.cs
+++ EFCore5WebApp.DAL/Migrations/20210428030545_AddLookUps.Designer.cs
@@ -3,14 +3,16 @@ using EFCore5WebApp.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EFCore5WebApp.DAL.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210428030545_AddLookUps")]
+    partial class AddLookUps
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
