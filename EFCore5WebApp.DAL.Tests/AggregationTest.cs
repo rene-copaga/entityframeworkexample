8dd673ffa673a12ecc670ad9d7d6acadd07a787e
Rene Copaga <rene.copaga@gmail.com>
2021-04-30 18:45:59 -0400

    Unit Test Setup

---
+++ EFCore5WebApp.DAL.Tests/AggregationTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace EFCore5WebApp.DAL.Tests
+{
+    [TestFixture]
+    public class AggregationTests
+    {
+        private AppDbContext _context;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _context = new AppDbContext(new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EfCore5WebApp; Trusted_Connection = True; MultipleActiveResultSets = true")
+                  .Options);
+        }
+    }
+}
