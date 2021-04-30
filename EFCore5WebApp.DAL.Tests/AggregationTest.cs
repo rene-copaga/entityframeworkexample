20026159f45aafe7f6ae586f44b380073f7cc562
Rene Copaga <rene.copaga@gmail.com>
2021-04-30 19:00:23 -0400

    Count

--- EFCore5WebApp.DAL.Tests/AggregationTest.cs
+++ EFCore5WebApp.DAL.Tests/AggregationTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
+using System.Linq;
 
 namespace EFCore5WebApp.DAL.Tests
 {
@@ -14,6 +15,21 @@ namespace EFCore5WebApp.DAL.Tests
             _context = new AppDbContext(new DbContextOptionsBuilder<AppDbContext>()
                 .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EfCore5WebApp; Trusted_Connection = True; MultipleActiveResultSets = true")
                   .Options);
+        }
+
+        [Test]
+        public void CountPersons()
+        {
+            int personCount = _context.Persons.Count();
+            Assert.AreEqual(2, personCount);
+        }
+
+        [Test]
+        public void CountPersonsWithFilter()
+        {
+            int personCount = _context.Persons.Count(x => x.FirstName == "John" &&
+            x.LastName == "Smith");
+            Assert.AreEqual(1, personCount);
         }
     }
 }
